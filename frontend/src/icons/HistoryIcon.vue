b9707b1df6903fea09787bb8601dbab2d36a683c
Daniel Di Tullio <danieldt2000@hotmail.com>
2025-12-25 12:38:58 -0300

    Diseñar mockup más realista de edición de mareas

---
+++ frontend/src/icons/HistoryIcon.vue
@@ -0,0 +1,32 @@
+<template>
+  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M12 8V12L15 15"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M3.05021 11C3.5009 7.05039 6.84577 4 10.8804 4C14.195 4 17.0652 5.99269 18.2882 8.79052"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M18 5V8.5H21.5"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M20.1196 13C19.6689 16.9496 16.324 20 12.2894 20C9.64571 20 7.32049 18.7291 5.88037 16.7629"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
+</template>
