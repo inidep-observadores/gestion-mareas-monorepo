3282ed5d59ec2cc78e68109bb13c35321fdfad9f
Daniel Di Tullio <danieldt2000@hotmail.com>
2026-02-09 00:06:02 -0300

    chore: eliminar archivos temporales y de depuración
    Eliminar logs de pruebas, scripts de diagnóstico y carpetas temporales en app/backend.

--- app/backend/debug_test.js
+++
@@ -1,7 +0,0 @@
-const { exec } = require('child_process');
-exec('npx jest src/mareas/mareas.service.spec.ts -t "should create first stage automatically"', { cwd: process.cwd() }, (error, stdout, stderr) => {
-    console.log('--- STDOUT ---');
-    console.log(stdout);
-    console.log('--- STDERR ---');
-    console.log(stderr);
-});
